214a954f44360e54aa6f3f3c27ba27b1fc52f55d
kanuos <sounakmukherjee@ymail.com>
2021-04-09 12:22:12 -0400

    css minified for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled : true,
+    content : ["./public/index.html"]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
